5bc9b9b6c78343b6c3ad13c483a8cdafdac9724a
SyedaBismaa <syedabisma173@gmail.com>
2025-08-02 23:09:30 +0530

    Ai integration completed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,34 +1,29 @@
-const jwt = require('jsonwebtoken')
-const userModel = require('../models/user.model')
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model")
 
-async  function authMiddleware(req,res,next){
+
+async function authMiddleware(req, res, next) {
     const token = req.cookies.token;
-
-    if(!token){
-        res.status(401).json({
-            message:"Unauthorized acess,please login first"
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, please login first"
         })
-      
-        try{
-      const decoded= jwt.verify(token , process.env.JWT_SECRET) 
-      const user = await userModel.findOne({
-        _id:decoded._id
-      })
-      req.user = user;
-      next();
-
-        }catch(err){
-            return res.status(401).json({
-                message:"Inavlid token, plase login again"
-            })
-
-        }
-
     }
 
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await userModel.findOne({
+            _id: decoded.id
+        })
 
+        req.user = user;
+        next()
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid token, please login again"
+        })
+    }
 
 }
 
-
-module.exports=authMiddleware;
+module.exports = authMiddleware;
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router();
-const authMiddleware = require('../middlewares/auth.middleware')
-const  createPostController = require('../controllers/post.controller')
+const authMiddleware = require("../middlewares/auth.middleware")
+const createPostController = require("../controllers/post.controller"); // ✅ Correct
+
+const multer = require("multer");
 
 const upload = multer({storage:multer.memoryStorage() })
 
-router.post('/', 
-    authMiddleware,
-    upload.single("image")
-     createPostController
-    )
+router.post('/',
+    authMiddleware, /* req.user = userData */
+    upload.single("image"),
+    createPostController
+)
 
-
-
-module.exports=router;
+module.exports = router;
